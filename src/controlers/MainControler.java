c2851a11231b7ee9ae829b972573346c1ff2d469
Shizenokami <theo.lognoz@telecomnancy.net>
2017-03-21 11:23:26 +0100

    Les bases: fenetre de base, about et Startwindow

---
+++ src/controlers/MainControler.java
@@ -0,0 +1,47 @@
+package controlers;
+
+import java.io.IOException;
+
+import app.controller.MainApp;
+import app.controller.StartWindowController;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
+public class MainControler {
+
+		private @FXML BorderPane Main_Pane;
+		
+		public void goHome(){
+			//System.out.println("test");
+			 try {
+		            FXMLLoader loader = new FXMLLoader();
+		            loader.setLocation(MainApp.class.getResource("../views/StartWindow.fxml"));
+		            AnchorPane StartWindow = (AnchorPane) loader.load();
+		            Main_Pane.setCenter(StartWindow);
+		            StartWindowController v = loader.getController();
+		            v.setmainpane(Main_Pane);
+		            
+		        } catch (IOException e) {
+		            e.printStackTrace();
+		        }
+		}
+		
+		public void exit(){
+			Platform.exit();
+		}
+		
+		public void goAbout(){
+			//System.out.println("test");
+			 try {
+		            FXMLLoader loader = new FXMLLoader();
+		            loader.setLocation(MainApp.class.getResource("../views/AboutWindow.fxml"));
+		            AnchorPane AboutWindow = (AnchorPane) loader.load();
+		            Main_Pane.setCenter(AboutWindow);
+		        } catch (IOException e) {
+		            e.printStackTrace();
+		        }
+		}
+}
---
+++ src/controlers/StartControler.java
@@ -0,0 +1,28 @@
+package controlers;
+
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
+public class StartControler {
+	
+	@FXML BorderPane Main_Pane;
+	@FXML TextField UsernameSI;
+	@FXML TextField UsernameSU;
+	@FXML TextField PassWordSI;
+	@FXML TextField PassWordSU;
+	@FXML TextField EmailAddr;
+	@FXML TextField ConfirmPassSU;
+	@FXML Button Button_SignIn;
+	@FXML Button Button_SignUp;
+	
+	public void setmainpane(BorderPane b){
+		this.Main_Pane=b;
+	}
+
+}
