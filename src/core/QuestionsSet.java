874f3893fd8e580479b19714b4f2461febf069bb
Romain <deniauxr@hotmail.fr>
2017-03-26 16:18:25 +0200

    set de Question

---
+++ src/core/QuestionsSet.java
@@ -0,0 +1,78 @@
+package core;
+
+import java.util.ArrayList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+public class QuestionsSet {
+
+	private NodeList set1;
+	private NodeList set2;
+	
+	public QuestionsSet() {}
+	
+	
+	//Path 1 "asset/File1.xml" Path 2 "asset/File2.xml"
+	public void loadQuestions(String path1,String path2){
+		
+		DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+		
+		try {
+			DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+			
+			Document doc=docBuilder.parse(path1);
+			set1 = doc.getElementsByTagName("question");
+			
+			doc=docBuilder.parse(path2);
+			set2 = doc.getElementsByTagName("question");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}	
+	}
+	
+	public Element getQuestion(int i,int set){
+		return (Element) getSet(set).item(i);
+	}
+	
+	public NodeList getSet(int set){
+		if (set==1)
+			return set1;
+		else
+			return set2;
+	}
+	
+	public ArrayList<Element> getRandomNumberQuestions(int i,int set){
+		ArrayList<Element> myQuestion=new ArrayList<Element>();
+		ArrayList<Integer> numbers=new ArrayList<Integer>();
+		
+		for (int l=0;l<i;l++){
+			int c=0;
+			int num=0;
+			do {
+				num =(int)(Math.random()*600-0.0001);
+				for (int k:numbers)
+					if (num==k)
+						c++;
+			} while  (c!=0);
+			myQuestion.add(getQuestion(num,set));
+			numbers.add(num);
+		}
+		return myQuestion;
+	}
+	
+	/* format quetions
+	 System.out.println("Question : " + getQuestion(i,set).getElementsByTagName("task").item(0).getTextContent());
+			System.out.println("Reponse A : " + getQuestion(i,set).getElementsByTagName("var").item(0).getTextContent());
+			System.out.println("Reponse B : " + getQuestion(i,set).getElementsByTagName("var").item(1).getTextContent());
+			System.out.println("Reponse C : " + getQuestion(i,set).getElementsByTagName("var").item(2).getTextContent());
+			System.out.println("Reponse D : " + getQuestion(i,set).getElementsByTagName("var").item(3).getTextContent());
+			System.out.println("Answer : " + getQuestion(i,set).getElementsByTagName("ans").item(0).getTextContent());
+			System.out.println("Explanation : " + getQuestion(i,set).getElementsByTagName("exp").item(0).getTextContent());
+			*/ 
+}
