60dde101a090da5904b00a9067252d2daed68079
Romain <deniauxr@hotmail.fr>
2017-03-29 19:28:59 +0200

    QCMChoice implemented

--- src/controlers/QCMChoiceControler.java
+++ src/controlers/QCMChoiceControler.java
@@ -2,8 +2,10 @@ package controlers;
 
 import java.io.IOException;
 import javafx.scene.control.Button;
+import core.QuestionSet;
 import core.QuestionsLoad;
 import core.Stat;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.ListView;
@@ -12,14 +14,13 @@ import javafx.scene.layout.BorderPane;
 
 public class QCMChoiceControler {
 	
-	private boolean start=true;
+	private boolean start=false;
 	private String choice;
-	
+	private Stat userstat=StartControler.userstat;
+    private QuestionsLoad db=new QuestionsLoad();
 	@FXML BorderPane Main_Pane;
 	@FXML ListView<String> choiceList;
 	@FXML Button Button_Start;
-	
-	
 	
 	public void clicStart() {
 		if (start) {
@@ -28,25 +29,33 @@ public class QCMChoiceControler {
 	}
 	public void setmainpane(BorderPane b){
 		this.Main_Pane=b;
+		choiceList.setItems((ObservableList<String>) userstat.getQCMList());
 	}
 	
 	
 	public void clicList() {
-		String choice = choiceList.getSelectionModel().getSelectedItem();
+		choice = choiceList.getSelectionModel().getSelectedItem();
 		start = true;
 		Button_Start.getStyleClass().add("rich-blue");
 	}
-	
+	public QuestionSet getQuestion(){
+		if (choice.equals("BIG TEST"))
+			return db.getRandomNumberQuestions(50,1);
+		for (int temp=2;temp<userstat.getQCMList().size()+1;temp++)
+			if (choice.equals(userstat.getTheme(temp)))
+				return db.getOneCategoryQuestions(temp,1);
+		return null;
+			
+	}
 	public void loadQCM() {
 		try {
-            QuestionsLoad db=new QuestionsLoad();
             FXMLLoader loader = new FXMLLoader();
             loader.setLocation(MainApp.class.getResource("../views/QCMQuestion.fxml"));
             AnchorPane StudentWindow = (AnchorPane) loader.load();
             Main_Pane.setCenter(StudentWindow);
             QCMControler v = loader.getController();
-            StartControler.userstat.resetLocal();
-            v.setParam(db.getRandomNumberQuestions(20,1),StartControler.userstat);
+            userstat.resetLocal();
+            v.setParam(getQuestion(),userstat);
             v.setmainpane(Main_Pane);
             
         } catch (IOException e) {
